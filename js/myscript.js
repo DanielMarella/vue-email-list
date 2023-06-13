8f9548cb1ad181e776efda54479d23661899e5ca
DanielMarella <marella.daniel96@gmail.com>
2023-06-13 15:54:51 +0200

    display in console the generation of one random email

--- js/myscript.js
+++ js/myscript.js
@@ -3,9 +3,35 @@ const { createApp } = Vue
     createApp({
         data() {
         return {
-            message: 'Hello Vue!'
+            message: 'Hello Vue!',
+
+            
+            emailGroup : [],
+        }
+    },
 
 
-        }
-        }
+
+
+    methods:{
+
+
+
+        getNewRandomEmail(){
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then(function (response){
+                const result = response.data;
+                console.log(result.response);
+            })
+        },
+
+
+    },
+
+    created(){
+        this.getNewRandomEmail();
+    }
+
+
 }).mount('#app')
+
